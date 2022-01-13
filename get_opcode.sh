b6a6089c2994dd9bd1f67a79ab7395c8c25894c4
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-01-13 02:42:35 +0000

    rename temp file in get_opcode.sh and remove when done this is to allow multiple users to use the script without permission issues

--- get_opcode.sh
+++ get_opcode.sh
@@ -1,2 +1,5 @@
-echo "$@" | aarch64-linux-gnu-as - -o /tmp/getopcode_bin \
-    && objdump /tmp/getopcode_bin -D | python3 get_opcode.py 
+tmpbin="/tmp/getopcode_bin_"$USER
+echo "$@" | aarch64-linux-gnu-as - -o "${tmpbin}" &&
+objdump "${tmpbin}" -D | python3 get_opcode.py
+
+rm "${tmpbin}"
