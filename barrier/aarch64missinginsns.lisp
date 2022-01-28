0009abd4e099f3f6bbe766c713d1d03264baf57a
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-01-28 05:33:13 +0000

    implement ORR (immediate) instructions this uses the decode-bit-masks function

--- barrier/aarch64missinginsns.lisp
+++ barrier/aarch64missinginsns.lisp
@@ -5,3 +5,8 @@
 
 (in-package aarch64)
 
+(defmacro ORR*ri (set rd rn imm)
+  (set rd (logor rn (immediate-from-bitmask imm))))
+
+(defun ORRWri (rd rn imm) (ORR*ri setw rd rn imm))
+(defun ORRXri (rd rn imm) (ORR*ri set$ rd rn imm))
