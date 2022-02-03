7f2f6e90664ac268f2ab9c1232a4e46847a15626
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-02-03 13:08:30 +0000

    separate plugin build and copying lisp files

--- barrier/autobuild.sh
+++ barrier/autobuild.sh
@@ -1,5 +1,2 @@
-bapbuild -pkgs ppx_bap,core_kernel,bap,bap-primus,bap-knowledge,monads barrier.plugin &&
-bapbundle install barrier.plugin &&
-searched_directory="${HOME}/.local/share/bap/primus/semantics" &&
-mkdir -p -v "$searched_directory" &&
-cp *.lisp "$searched_directory"
+./build_plugin.sh
+./copy_lisp_files.sh
---
+++ barrier/build_plugin.sh
@@ -0,0 +1,2 @@
+bapbuild -pkgs ppx_bap,core_kernel,bap,bap-primus,bap-knowledge,monads barrier.plugin &&
+bapbundle install barrier.plugin
