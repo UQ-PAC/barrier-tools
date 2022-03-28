ba811ce44bc8084d8fdec2309db9fe7d2e2c0090
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-03-28 03:09:38 +0000

    modify scripts to streamline lifter testing

--- get_opcode.sh
+++ get_opcode.sh
@@ -5,8 +5,14 @@ initial_dir=$(pwd)
 tmp_bin="/tmp/getopcode_bin_"$USER
 
 cd "${script_dir}"
-echo "$@" | aarch64-linux-gnu-as - -o "${tmp_bin}" &&
-objdump "${tmp_bin}" -D | python3 get_opcode.py
 
+echo "$@" | aarch64-linux-gnu-as -march=armv8.5-a - -o "${tmp_bin}"
+return_code=$?
+if [ "$return_code" -ne 0 ]; then
+    cd "${initial_dir}"
+    exit "$return_code"
+fi
+
+objdump "${tmp_bin}" -D | python3 get_opcode.py &&
 rm "${tmp_bin}"
 cd "${initial_dir}"
---
+++ lift_instruction.sh
@@ -0,0 +1,46 @@
+# All arguments except the last are provided to bap-mc,
+# and the last is the asm to be run through get_opcode.sh.
+#
+# Examples:
+# ./lift_instruction "mov x0, x0"
+# ./lift_instruction -show-knowledge "mov x0, x0"
+
+# gets directory that this script is in
+script_dir=$(dirname "$(readlink -f "$0")")
+initial_dir=$(pwd)
+
+# https://stackoverflow.com/a/3352015
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"   
+    printf '%s' "$var"
+}
+
+# https://stackoverflow.com/a/33271194
+instruction=$(trim ${@:$#}) # last parameter 
+bap_mc_options=$(trim ${*%${!#}}) # all parameters except the last
+
+cd "$script_dir"
+
+opcode=$(./get_opcode.sh "$instruction")
+opcode_return_code=$?
+if [ "$opcode_return_code" -ne 0 ]; then
+    cd "$initial_dir"
+    exit "$opcode_return_code"
+fi
+
+opcode=$(trim "$opcode")
+
+echo "Instruction: $instruction"
+echo "Opcode: $opcode"
+
+if [ -z "$bap_mc_options" ]; then
+    bap-mc --show-bil --arch=aarch64 -- $opcode
+else
+    bap-mc --show-bil --arch=aarch64 "$bap_mc_options" -- $opcode
+fi
+
+cd "$initial_dir"
