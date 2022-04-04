6dd79380bd8208753287f4d24ae6a5fef878d874
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-04-04 01:16:03 +0000

    generate local user directories if they don't exist

--- extract_lisp_from_bap.sh
+++ extract_lisp_from_bap.sh
@@ -10,6 +10,8 @@ else
 fi
 searched_dir="${HOME}/.local/share/bap/primus"
 
+mkdir -p ${searched_dir}/{semantics,lisp}
+
 cp ${bap_src_dir}/plugins/arm/semantics/*.lisp ${searched_dir}/semantics
 cp ${bap_src_dir}/plugins/primus_lisp/semantics/bits.lisp ${searched_dir}/semantics
 cp ${bap_src_dir}/plugins/primus_lisp/lisp/*.lisp ${searched_dir}/lisp
